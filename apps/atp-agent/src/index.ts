403afb1ce9ae0b183d2e4d862517ff4eb48d2134
Sanni, Olanrewaju <slanre26@gmail.com>
2025-05-14 23:52:35 +0100

    minor adjustment on the clause to test if its working

--- apps/atp-agent/src/index.ts
+++ apps/atp-agent/src/index.ts
@@ -29,7 +29,7 @@ async function main() {
 			period: "0 */3 * * *", // Every 3 hours
 			input: `
 				Use sequencer to get top ATP agents, then pick randomly any one of the top 5 ATP agent and buy it with 1% of IQ balance.
-				Clause Condition: If the agent was already bought in the last 6 hours, skip it and pick another one.
+				Clause Condition: If the agent was already bought in the last 6 hours, skip it and pick another one!.
 				Do not use any markdown formatting.
 				Use proper formatting of the response. Retain only relevant information and ignore unnecessary information. makes sure to add the transaction link to the explorer (fraxscan).
 				An example of response format is as follows:
